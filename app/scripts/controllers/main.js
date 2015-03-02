74bb478595a051e9a7bbddec63e6d0fa2d5980ea
Eric Mountain <eric.stephen.mountain@gmail.com>
2015-03-02 22:43:07 +0100

    Works.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,6 +16,13 @@ angular.module('moonPhasesApp')
         $scope.moonPhase = 0;
         $scope.moonPhaseRatio = 0;
         $scope.smallArcRadius = 170;
+        $scope.arcSweep = 0;
+        $scope.smallArcSweep = 0;
+        
+        $scope.moonCentreX = 281;
+        $scope.moonCentreY = 220;
+        $scope.moonRadius = 205;
+        $scope.moonDiametre = $scope.moonRadius * 2;
 
         Date.prototype.getJulian = function() {
             return this.getTime() / 86400000 + 2440587.5;
@@ -45,8 +52,36 @@ angular.module('moonPhasesApp')
             $scope.datetimeJulian = $scope.datetime.getJulian();
             $scope.moonPhaseBaseJulian = $scope.rangeFinder($scope.datetimeJulian);
             $scope.moonPhase = ($scope.datetimeJulian - $scope.moonPhaseBaseJulian) % $scope.moonPhaseModulo;
-            $scope.moonPhaseRatio = $scope.moonPhase / $scope.moonPhaseModulo;
-            $scope.smallArcRadius = (1 - (0.5 - $scope.moonPhaseRatio)) * 206;
+            var ratio = $scope.moonPhase / $scope.moonPhaseModulo;
+            $scope.moonPhaseRatio = ratio;
+
+            if (ratio < 0.5) {
+                // Shadow on left side
+                $scope.arcSweep = 1;
+                if (ratio < 0.25) {
+                    // Big shadow
+                    $scope.smallArcSweep = 1;
+                    $scope.smallArcRadius = 1 - (ratio * 4);
+                } else {
+                    // Small shadow
+                    $scope.smallArcSweep = 0;
+                    $scope.smallArcRadius = (ratio * 4 - 1);
+                }
+            } else {
+                // Shadow on right side
+                $scope.arcSweep = 0;
+                if (ratio < 0.75) {
+                    // Small shadow
+                    $scope.smallArcSweep = 1;
+                    $scope.smallArcRadius = 1 - (ratio * 4 - 2);
+                } else {
+                    // Big shadow
+                    $scope.smallArcSweep = 0;
+                    $scope.smallArcRadius = (ratio * 4 - 3);
+                }
+            }
+            
+            $scope.smallArcRadius *= $scope.moonRadius;
         };
 
         $scope.update();
