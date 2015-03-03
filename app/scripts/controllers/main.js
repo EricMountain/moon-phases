c4c2538e93fc71345653c358b20da7bc809aa39e
Eric Mountain <eric.stephen.mountain@gmail.com>
2015-03-03 12:01:37 +0100

    Modularising.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,13 +11,9 @@ angular.module('moonPhasesApp')
     .controller('MainCtrl', ['$scope', function ($scope) {
         var moon = window.moon;
         $scope.datetime = new Date();
-        $scope.datetimeJulian = 0;
-        $scope.moonPhaseModulo = 29.5305888610;
-        $scope.moonPhaseBaseJulian = 0;
-        $scope.moonPhase = 0;
-        $scope.moonPhaseRatio = 0;
-        $scope.smallArcRadius = 170;
+
         $scope.arcSweep = 0;
+        $scope.smallArcRadius = 0;
         $scope.smallArcSweep = 0;
         
         $scope.moonCentreX = 281;
@@ -25,30 +21,8 @@ angular.module('moonPhasesApp')
         $scope.moonRadius = 205;
         $scope.moonDiametre = $scope.moonRadius * 2;
 
-        Date.prototype.getJulian = function() {
-            return this.getTime() / 86400000 + 2440587.5;
-        };
-
-        $scope.rangeFinder = function(datetimeJulian) {
-            var n = 0;
-            var dN = moon.newMoonFinder(n);
-            var dNPlusOne = moon.newMoonFinder(n + 1);
-            
-            while (dN > datetimeJulian || dNPlusOne < datetimeJulian) {
-                n += 1;
-                dN = moon.newMoonFinder(n);
-                dNPlusOne = moon.newMoonFinder(n + 1);
-            }
-
-            return dN;
-        };
-
         $scope.update = function() {
-            $scope.datetimeJulian = $scope.datetime.getJulian();
-            $scope.moonPhaseBaseJulian = $scope.rangeFinder($scope.datetimeJulian);
-            $scope.moonPhase = ($scope.datetimeJulian - $scope.moonPhaseBaseJulian) % $scope.moonPhaseModulo;
-            var ratio = $scope.moonPhase / $scope.moonPhaseModulo;
-            $scope.moonPhaseRatio = ratio;
+            var ratio = moon.calculatePhaseRatio($scope.datetime);
 
             if (ratio < 0.5) {
                 // Shadow on left side
--- app/scripts/moon.js
+++ app/scripts/moon.js
@@ -2,13 +2,42 @@
 
 /* global: moon: true */
 window.moon = (function() {
+    Date.prototype.getJulian = function() {
+        return this.getTime() / 86400000 + 2440587.5;
+    };
+
+    function newMoonFinder(n) {
+        var julian20000101 = new Date(Date.UTC(2000, 0, 1, 0, 0, 0, 0)).getJulian();
+        
+        return 5.597661 + 29.5305888610 * n + (102.026 * Math.pow(10, -12)) * n * n - 0.000739 - (235 * Math.pow(10, -12)) * n * n + julian20000101;
+    }
+    
+    function findPreviousNewMoon(datetimeJulian) {
+        var n = 0;
+        var dN = newMoonFinder(n);
+        var dNPlusOne = newMoonFinder(n + 1);
+        
+        while (dN > datetimeJulian || dNPlusOne < datetimeJulian) {
+            n += 1;
+            dN = newMoonFinder(n);
+            dNPlusOne = newMoonFinder(n + 1);
+        }
+        
+        return dN;
+    }
+    
     // http://en.wikipedia.org/wiki/New_moon#Determining_new_moons:_an_approximate_formula
     return {
-        newMoonFinder: function(n) {
-            var julian20000101 = new Date(Date.UTC(2000, 0, 1, 0, 0, 0, 0)).getJulian();
-            
-            return 5.597661 + 29.5305888610 * n + (102.026 * Math.pow(10, -12)) * n * n - 0.000739 - (235 * Math.pow(10, -12)) * n * n + julian20000101;
-               }
+        calculatePhaseRatio: function(datetime) {
+            var moonPhaseModulo = 29.5305888610;
+            var datetimeJulian = datetime.getJulian();
+            var moonPhaseBaseJulian = findPreviousNewMoon(datetimeJulian);
+            var moonPhase = (datetimeJulian - moonPhaseBaseJulian) % moonPhaseModulo;
+            var ratio = moonPhase / moonPhaseModulo;
+
+            return ratio;
+        }
+
     };
 })();
 
