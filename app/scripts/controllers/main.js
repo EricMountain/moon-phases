8fe0003664b2b93d62b55010422dc4f3ab0d4da5
Eric Mountain <eric.stephen.mountain@gmail.com>
2015-03-03 12:20:45 +0100

    Shadow path computation isolated off.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,45 +12,18 @@ angular.module('moonPhasesApp')
         var moon = window.moon;
         $scope.datetime = new Date();
 
-        $scope.arcSweep = 0;
-        $scope.smallArcRadius = 0;
-        $scope.smallArcSweep = 0;
-        
         $scope.moonCentreX = 281;
         $scope.moonCentreY = 220;
         $scope.moonRadius = 205;
-        $scope.moonDiametre = $scope.moonRadius * 2;
 
+        $scope.shadowPath = '';
+        
         $scope.update = function() {
-            var ratio = moon.calculatePhaseRatio($scope.datetime);
+            $scope.shadowPath = moon.buildShadowPath($scope.moonCentreX,
+                                                     $scope.moonCentreY,
+                                                     $scope.moonRadius,
+                                                     $scope.datetime);
 
-            if (ratio < 0.5) {
-                // Shadow on left side
-                $scope.arcSweep = 1;
-                if (ratio < 0.25) {
-                    // Big shadow
-                    $scope.smallArcSweep = 1;
-                    $scope.smallArcRadius = 1 - (ratio * 4);
-                } else {
-                    // Small shadow
-                    $scope.smallArcSweep = 0;
-                    $scope.smallArcRadius = (ratio * 4 - 1);
-                }
-            } else {
-                // Shadow on right side
-                $scope.arcSweep = 0;
-                if (ratio < 0.75) {
-                    // Small shadow
-                    $scope.smallArcSweep = 1;
-                    $scope.smallArcRadius = 1 - (ratio * 4 - 2);
-                } else {
-                    // Big shadow
-                    $scope.smallArcSweep = 0;
-                    $scope.smallArcRadius = (ratio * 4 - 3);
-                }
-            }
-            
-            $scope.smallArcRadius *= $scope.moonRadius;
         };
 
         $scope.update();
--- app/scripts/moon.js
+++ app/scripts/moon.js
@@ -1,15 +1,17 @@
 'use strict';
 
-/* global: moon: true */
 window.moon = (function() {
     Date.prototype.getJulian = function() {
         return this.getTime() / 86400000 + 2440587.5;
     };
 
+    // http://en.wikipedia.org/wiki/New_moon#Determining_new_moons:_an_approximate_formula
     function newMoonFinder(n) {
         var julian20000101 = new Date(Date.UTC(2000, 0, 1, 0, 0, 0, 0)).getJulian();
         
-        return 5.597661 + 29.5305888610 * n + (102.026 * Math.pow(10, -12)) * n * n - 0.000739 - (235 * Math.pow(10, -12)) * n * n + julian20000101;
+        return 5.597661 + 29.5305888610 * n + (102.026 * Math.pow(10, -12)) * n * n -
+        0.000739 - (235 * Math.pow(10, -12)) * n * n +
+        julian20000101;
     }
     
     function findPreviousNewMoon(datetimeJulian) {
@@ -25,20 +27,69 @@ window.moon = (function() {
         
         return dN;
     }
-    
-    // http://en.wikipedia.org/wiki/New_moon#Determining_new_moons:_an_approximate_formula
-    return {
-        calculatePhaseRatio: function(datetime) {
-            var moonPhaseModulo = 29.5305888610;
-            var datetimeJulian = datetime.getJulian();
-            var moonPhaseBaseJulian = findPreviousNewMoon(datetimeJulian);
-            var moonPhase = (datetimeJulian - moonPhaseBaseJulian) % moonPhaseModulo;
-            var ratio = moonPhase / moonPhaseModulo;
 
-            return ratio;
+    function calculatePhaseRatio(datetime) {
+        var moonPhaseModulo = 29.5305888610;
+        var datetimeJulian = datetime.getJulian();
+        var moonPhaseBaseJulian = findPreviousNewMoon(datetimeJulian);
+        var moonPhase = (datetimeJulian - moonPhaseBaseJulian) % moonPhaseModulo;
+        var ratio = moonPhase / moonPhaseModulo;
+        
+        return ratio;
+    }
+
+    function buildShadowPathInternal(centreX, centreY, radius, datetime) {
+        var ratio = calculatePhaseRatio(datetime);
+        var arcSweep = 0;
+        var smallArcRadius = 0;
+        var smallArcSweep = 0;
+        
+        if (ratio < 0.5) {
+            // Shadow on left side
+            arcSweep = 1;
+            if (ratio < 0.25) {
+                // Big shadow
+                smallArcSweep = 1;
+                smallArcRadius = 1 - (ratio * 4);
+            } else {
+                // Small shadow
+                smallArcSweep = 0;
+                smallArcRadius = (ratio * 4 - 1);
+            }
+        } else {
+            // Shadow on right side
+            arcSweep = 0;
+            if (ratio < 0.75) {
+                // Small shadow
+                smallArcSweep = 1;
+                smallArcRadius = 1 - (ratio * 4 - 2);
+            } else {
+                // Big shadow
+                smallArcSweep = 0;
+                smallArcRadius = (ratio * 4 - 3);
+            }
         }
+        
+        smallArcRadius *= radius;
 
+        var diametre = radius * 2;
+
+        /* Move to centre.
+         * Move to bottom of moon.
+         * Arc from bottom to top of moon, with moon's radius.
+         * Arc from top to bottom of moon, with reduced radius.
+         */
+        var path = 'M' + centreX + ',' + centreY;
+        path += ' m0,' + radius;
+        path += ' a' + radius + ',' + radius + ' 0 0,' + arcSweep + ' 0,-' + diametre;
+        path += ' a' + smallArcRadius + ',' + radius + ' 0 0,' + smallArcSweep + ' 0,' + diametre;
+
+        return path;
+    }
+    
+    return {
+        buildShadowPath: function(centreX, centreY, radius, datetime) {
+            return buildShadowPathInternal(centreX, centreY, radius, datetime);
+        }
     };
 })();
-
-
