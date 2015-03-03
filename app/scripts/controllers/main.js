75986bf29d1d29bc4e141a19c2ea47c2679634ac
Eric Mountain <eric.stephen.mountain@gmail.com>
2015-03-04 00:29:36 +0100

    Skeleton, not for merging to master.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,38 +8,12 @@
  * Controller of the moonPhasesApp
  */
 angular.module('moonPhasesApp')
-    .controller('MainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    .controller('MainCtrl', ['$scope', function ($scope) {
 
         var moon = window.moon;
 
-        $scope.update = function() {
-            $scope.shadowPath = moon.buildShadowPath($scope.moonCentreX,
-                                                     $scope.moonCentreY,
-                                                     $scope.moonRadius,
-                                                     $scope.datetime);
-        };
-
-        $scope.toggleAnimate = function() {
-            (function x() {
-                if ($scope.animate) {
-                    $scope.datetime = new Date($scope.datetime.getTime() + 3600 * 1000 * 2);
-                    $scope.update();
-                    $timeout(x, 60);
-                }
-            })();
-        };
-
         $scope.now = function() {
-            $scope.datetime = new Date();
-            $scope.animate = false;
-            $scope.update();
         };
-
-        $scope.moonCentreX = 281;
-        $scope.moonCentreY = 220;
-        $scope.moonRadius = 205;
-
-        $scope.shadowPath = '';
 
         $scope.now();
   }]);
